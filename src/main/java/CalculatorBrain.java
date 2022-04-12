3ccdcc5be8c865b1daf82795d2eb157280400525
luma <luma2906@gmail.com>
2022-04-12 16:47:05 -0500

    first commit

---
+++ src/main/java/CalculatorBrain.java
@@ -0,0 +1,18 @@
+public class CalculatorBrain {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtrat(int a, int b) {
+        return a + b;
+    }
+
+    public int divide(int a, int b) {
+        return a/b;
+    }
+
+    public int multiply(int a, int b){
+        return a * b;
+    }
+}
